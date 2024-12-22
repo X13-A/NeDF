b412510026cbe3d3b76018acd8803ac455ca7df9
Alex <foulonalex@yahoo.com>
2024-12-22 05:25:03 -0500

    Change files structure

--- archive/settings.py
+++
@@ -1,8 +0,0 @@
-# Camera settings
-CAMERA_FOV = 120.0       # Field of view in radians (45 degrees)
-CAMERA_ASPECT_RATIO = 16.0 / 9.0     # Aspect ratio (width / height)
-CAMERA_NEAR = 0.1                    # Near clipping plane
-CAMERA_FAR = 100.0                   # Far clipping plane
-
-GRID_WIDTH = 320
-GRID_HEIGHT = 320
